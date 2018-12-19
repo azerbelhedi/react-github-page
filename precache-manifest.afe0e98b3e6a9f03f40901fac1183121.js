7141ca349268213b59d5e37f52304f033171ec73
azerbelhedi <azerbelhedide@gmail.com>
2018-12-19 22:39:28 +0100

    Updates

---
+++ precache-manifest.afe0e98b3e6a9f03f40901fac1183121.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e564a2f37186d8a86cac",
+    "url": "/react-github-page/static/js/runtime~main.e564a2f3.js"
+  },
+  {
+    "revision": "ab6de1545f76e3374c76",
+    "url": "/react-github-page/static/js/main.ab6de154.chunk.js"
+  },
+  {
+    "revision": "9fcd1b0ff83dba862953",
+    "url": "/react-github-page/static/js/1.9fcd1b0f.chunk.js"
+  },
+  {
+    "revision": "4d9952182d429f965eef0f3dda6e5625",
+    "url": "/react-github-page/index.html"
+  }
+];
